9034889f8ea8a1263afd5387c9717ac4626db8f4
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2020-05-03 20:32:57 -0700

    set up tables

--- migrations/20200502215245_create_lists_table.ts
+++ migrations/20200502215245_create_lists_table.ts
@@ -3,14 +3,29 @@ import * as Knex from 'knex'
 module.exports.up = async function up(knex: Knex): Promise<any> {
   await knex.raw(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`)
 
-  await knex.raw(`CREATE TABLE lists (
-    id uuid DEFAULT uuid_generate_v4(),
-    name text NOT NULL,
-    "colorScheme" text,
-    PRIMARY KEY (id)
-  );`)
+  await knex.raw(`
+    CREATE TABLE lists (
+      id uuid DEFAULT uuid_generate_v4(),
+      name text NOT NULL,
+      color_scheme text,
+      PRIMARY KEY (id)
+    );
+  `)
+
+  await knex.raw(`
+    CREATE TABLE list_items (
+      id uuid DEFAULT uuid_generate_v4(),
+      list_id uuid NOT NULL,
+      name text NOT NULL,
+      status text DEFAULT 'todo',
+      description text,
+      PRIMARY KEY (id, list_id),
+      FOREIGN KEY (list_id) REFERENCES lists(id) ON DELETE CASCADE
+    );
+  `)
 }
 
 module.exports.down = async function down(knex: Knex): Promise<any> {
-  await knex.raw('DROP TABLE lists;')
+  await knex.raw(`DROP TABLE list_items;`)
+  await knex.raw(`DROP TABLE lists;`)
 }
