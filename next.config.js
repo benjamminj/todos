c5d6b7f2899847ac3840f750d06d81c9f9fa5419
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2020-05-08 22:41:29 -0700

    update config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   env: {
-    BASE_API_URL: 'https://' + process.env.VERCEL_URL,
+    BASE_API_URL: process.env.BASE_API_URL,
     DATABASE_URL: process.env.DATABASE_URL,
   },
   typescript: {
