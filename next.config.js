c6c0eb3530f90bd6a827f00290976fe86520124f
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2020-05-08 21:27:33 -0700

    update env variable

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   env: {
-    BASE_API_URL: process.env.BASE_API_URL,
+    BASE_API_URL: process.env.VERCEL_URL,
     DATABASE_URL: process.env.DATABASE_URL,
   },
   typescript: {
