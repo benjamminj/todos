0abe72fb8c323e3245ba2b7ab7e09bcd20a028fb
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2020-05-16 11:15:40 -0700

    fix url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 module.exports = {
   target: 'server',
   env: {
-    BASE_API_HOST: process.env.PROTOCOL + '://' + process.env.VERCEL_URL,
+    BASE_API_URL: process.env.PROTOCOL + '://' + process.env.VERCEL_URL,
     DATABASE_URL: process.env.DATABASE_URL,
     FAUNADB_KEY: process.env.FAUNADB_KEY,
   },
