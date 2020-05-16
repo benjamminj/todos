f59ccfe71b11629c35541d5fffa92d49aee4d5c4
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2020-05-15 21:22:09 -0700

    try fetch fix

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -4,4 +4,4 @@ const isServer = typeof window === 'undefined'
 const prefix = isServer ? process.env.BASE_API_URL : ''
 
 export const fetch = (url: string, options?: RequestInit) =>
-  unfetch(prefix + url, options)
+  isServer ? unfetch(prefix + url, options) : window.fetch(url, options)
